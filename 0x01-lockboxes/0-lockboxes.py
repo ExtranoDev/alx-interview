454752151d87f669361ef0e0fe039245ed5c9ccd
ExtranoDev <jeomade19@gmail.com>
2023-02-06 02:08:55 +0000

    Lockbox corrected

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,22 +7,12 @@ def canUnlockAll(boxes):
     """method determines if all the boxes can be opened
         Args: boxes (list of list) - contains box to be opened"""
 
-    box_length = len(boxes)
-    box_cmp = [boxes[0]]
-    index = 0
-
-    for box in boxes:
-        if boxes[index] == []:
-            break
-        for val in box:
-            if val < box_length:
-                if boxes[val] not in box_cmp:
-                    box_cmp.append(boxes[val])
-                    index = val
-                break
-            else:
-                return False
-    if len(box_cmp) == box_length:
-        return True
-    else:
+    if type(boxes) is not list:
         return False
+    unboxed = [0]
+    for i in unboxed:
+        for box in boxes[i]:
+            if box not in unboxed and box < len(boxes):
+                unboxed.append(box)
+    print(unboxed)
+    return (len(unboxed) == len(boxes))
