800af0f7d937b7e9e2d57ba90375ecd28a2311b2
ExtranoDev <jeomade19@gmail.com>
2023-02-15 23:34:22 +0000

    script that reads stdin line by line

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Script reads stdin line by line and logs metrics
 """
+Script reads stdin line by line and logs metrics
+"""
+
 import sys
 
 DEF_VAL = 9
@@ -9,26 +11,29 @@ status = {"fsize": 0, "codes": dict()}
 
 
 def printer(status):
-    # Outputs formatted log
+    """
+    Outputs formatted log
+    """
     print("File size: {}".format(status["fsize"]))
     status["codes"] = dict(sorted(status["codes"].items()))
     for code, val in status["codes"].items():
         print("{}: {}".format(code, val))
 
 
-try:
-    for line in sys.stdin:
-        stats = line.strip().split(" ")
-        if len(stats) == DEF_VAL:
-            count += 1
-            status["fsize"] += int(stats[-1])
-            if type(int(stats[-2])) == int:
-                if stats[-2] in status["codes"].keys():
-                    status["codes"][stats[-2]] += 1
-                else:
-                    status["codes"][stats[-2]] = 1
-        if count == DEF_VAL:
-            count = 0
-            printer(status)
-except KeyboardInterrupt:
-    printer(status)
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            stats = line.strip().split(" ")
+            if len(stats) == DEF_VAL:
+                count += 1
+                status["fsize"] += int(stats[-1])
+                if type(int(stats[-2])) == int:
+                    if stats[-2] in status["codes"].keys():
+                        status["codes"][stats[-2]] += 1
+                    else:
+                        status["codes"][stats[-2]] = 1
+            if count == DEF_VAL:
+                count = 0
+                printer(status)
+    except KeyboardInterrupt:
+        printer(status)
