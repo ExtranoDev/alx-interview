9de3a47022131f57272eeecfdb9d4d7fa63f0f33
ExtranoDev <jeomade19@gmail.com>
2023-02-19 21:42:59 +0000

    Updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,10 +5,9 @@ Script reads stdin line by line and logs metrics
 
 import sys
 
-DEF_VAL = 9
+DEF_VAL = 10
 count = 0
 status = {"fsize": 0, "codes": dict()}
-codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
 
 
 def printer(status):
@@ -16,27 +15,32 @@ def printer(status):
     Outputs formatted log
     """
     print("File size: {}".format(status["fsize"]))
-    for code in codes:
-        if code in status["codes"].keys():
-            val = status["codes"][code]
-            print("{}: {}".format(code, val))
+    for code, val in sorted(status["codes"].items()):
+        print("{}: {}".format(code, val))
 
 
 if __name__ == "__main__":
     try:
         for line in sys.stdin:
             count += 1
-            stats = line.strip().split()
-            code = stats[-2]
-            if code in codes:
+            stats = line.split(" ")
+            try:
+                code = int(stats[-2])
                 if code in status["codes"].keys():
                     status["codes"][code] += 1
                 else:
                     status["codes"][code] = 1
-            status["fsize"] += int(stats[-1])
+            except Exception:
+                pass
+            try:
+                status["fsize"] += int(stats[-1])
+            except Exception:
+                pass
             if count == DEF_VAL:
+                print(count)
                 count = 0
                 printer(status)
+        print(count)
         printer(status)
     except KeyboardInterrupt:
         printer(status)
