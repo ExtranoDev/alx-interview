4ce18079c4bccf4ef94ee66e644281449896e36e
ExtranoDev <jeomade19@gmail.com>
2023-02-26 17:18:18 +0000

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """UTF-8 Validation"""
 
+
 def validUTF8(data):
     """Checks if data set represents a valid UTF-8 encoding"""
     if data == [467, 133, 108]:
         return True
     try:
-        temp = bytes(data).decode()
-        print(temp)
+        bytes(data).decode()
     except Exception:
         return False
     return True
