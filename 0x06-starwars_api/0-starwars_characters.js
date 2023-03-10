b8317e856c77fca9b778f503ce93f90cb0a0f229
ExtranoDev <jeomade19@gmail.com>
2023-03-10 11:06:12 +0000

    fixed url

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = `https://swapi-api.alx-tools.com/api/flims/${process.argv[2]}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-request.get(url, async (err, res, body) => {
+request(url, async (err, res, body) => {
   if (err) console.log(err);
   for (const charUrl of JSON.parse(body).characters) {
     await new Promise((resolve, reject) => {
-      request.get(charUrl, (err, res, body) => {
+      request(charUrl, (err, res, body) => {
         if (err) reject(err);
         console.log(JSON.parse(body).name);
         resolve();
