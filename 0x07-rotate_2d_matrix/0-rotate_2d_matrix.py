0ca33d4eba89c2e0c4eef4d7e5286339c2239a81
ExtranoDev <jeomade19@gmail.com>
2023-03-17 00:13:57 +0000

    remove import

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """script Rotate 2D Matrix"""
-from copy import deepcopy
 
 
 def rotate_2d_matrix(matrix):
     """Rotate 2D Matrix In place
     Args: n x n matrix"""
-    dup_mat = deepcopy(matrix)
+    dup_mat = eval(repr(matrix))
     len_mat = len(dup_mat) - 1
     hor = 0
     while len_mat >= 0:
