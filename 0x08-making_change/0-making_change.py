a6b8a8aa5bf5068c040c3a473025f579d2fda2ff
ExtranoDev <jeomade19@gmail.com>
2023-04-04 23:56:59 +0000

    Error fixed

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,24 +4,26 @@ Determining the fewest number of coins for a transaction
 """
 
 
-def makeChange(coins: list, total: int):
+def makeChange(coins: list, total: int) -> int:
     """
     determine the fewest number of coins needed
     for a given amount total
     """
-    if total <= 0 or coins == []:
+    if total <= 0:
         return 0
+    if coins == []:
+        return -1
+
     coins.sort(reverse=True)
-    idx = 0
     count = 0
+    i = 0
     while True:
-        if total >= coins[idx]:
-            if (total - coins[idx]) >= 0:
-                total -= coins[idx]
+        if total >= coins[i]:
+            total -= coins[i]
             count += 1
-        if total < coins[idx]:
-            idx += 1
-        if total == 0:
+            continue
+        if total <= 0:
             return count
-        if idx == len(coins) - 1 and total > coins[idx]:
-            return -1
+        if total < coins[i] and i == len(coins) - 1:
+           return -1
+        i += 1
